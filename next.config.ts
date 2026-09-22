fe0ca26cd87b2df52a86a63f30c16750ee4074db
Narain-karthick <240146.ad@rmkec.ac.in>
2026-09-22 09:51:41 +0530

    Microphone added

--- next.config.ts
+++ next.config.ts
@@ -33,7 +33,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(self), microphone=(), geolocation=(self)',
+            value: 'camera=(self), microphone=(self), geolocation=(self)',
           },
         ],
       },
